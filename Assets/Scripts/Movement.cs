65eac79ab5b0847cb27913713ff8bedd83f2bcf2
Corey Jordan <113132164+CoreyJordan@user.noreply.github.com>
2022-11-05 18:34:03 -0400

    First commit, initial project and .gitignore

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    float thrustSpeed = 1000f;             // Rate of thrust
+    float rotateRate = 100f;               // Rate of rotation
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+
+    void ProcessThrust()
+    {
+        // Press space bar to thrust.
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * thrustSpeed * Time.deltaTime);
+        }  
+    }
+
+    void ProcessRotation()
+    {
+        // Press A to rotate left.
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotateRate);
+        }
+        // Press D to rotate right.
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotateRate);
+        }
+    }
+
+     void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true;    // Freezing rotation so we can manually rotate.
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false;
+    }
+}
