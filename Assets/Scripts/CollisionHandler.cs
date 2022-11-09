91ccf962d48d68e9349d667e8cc5b202939b5f86
Corey Jordan <113132164+CoreyJordan@user.noreply.github.com>
2022-11-09 18:08:01 -0500

    Add Fly Under Wall scene and loop levels

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -12,17 +12,31 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("Bumped friendly.");
                 break;
             case "Finish":
-                Debug.Log("Congrats.");
+                LoadNextLevel();
                 break;
             default:
                 ReloadLevel();
                 break;
         }
     }
-    
+
     void ReloadLevel()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void LoadNextLevel()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
+        
     }
 }
