36b816066e28fca16ed6bb4b97a59092125bcfc2
Corey Jordan <113132164+CoreyJordan@user.noreply.github.com>
2022-11-11 20:19:29 -0500

    Add Invoke and StartCrashSequence, LoadNextLevel

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -4,30 +4,54 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+
+    [SerializeField] float delay = 1f;
+
     void OnCollisionEnter(Collision other)
     {
+        // Reload on crash, load next level on finish.
         switch (other.gameObject.tag)
         {
             case "Friendly":
                 Debug.Log("Bumped friendly.");
                 break;
             case "Finish":
-                LoadNextLevel();
+                StartNextLevelSequence();
                 break;
             default:
-                ReloadLevel();
+                StartCrashSequence();
                 break;
         }
     }
 
+    void StartNextLevelSequence()
+    {
+        // Disabled flight controls, wait, and load next level.
+        // todo add sound effect on crash
+        // todo add particle effect on crash
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", delay);
+    }
+
+    void StartCrashSequence()
+    {
+        // Disable flight controls, pause, and reload level.
+        // Add sound effect on finish.
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", delay);
+
+    }
+
     void ReloadLevel()
     {
+        // Reload the current level.
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
     }
 
     void LoadNextLevel()
     {
+        // Load next level.
         int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
         if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
         {
